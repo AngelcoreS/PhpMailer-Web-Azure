54585d8bd3a50dabb754f490fcdfa0c1b3d08a93
Angel Perez <124532714+AngelcoreS@users.noreply.github.com>
2024-09-13 14:35:11 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copy custom Nginx config to /etc/nginx
-cp /home/default /etc/nginx/sites-available/default
+cp /home/site/includes /etc/nginx/sites-available/default
 # Restart Nginx service
 service nginx restart
 
